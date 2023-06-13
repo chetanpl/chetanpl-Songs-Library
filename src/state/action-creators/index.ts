24b43de3fe2328b416737a9c4146a1e7d88d691c
Chetan Pal <chetanpl@gmail.com>
2023-06-13 23:24:29 +0100

    Added dynamic URL links to hit songs collection library

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -3,37 +3,39 @@ import { ActionType } from '../actionTypes';
 import { Action } from '../actions';
 import { Dispatch } from 'redux';
 
-export const searchRepositories  = (term : string, numberOfrecord:number,country:string) => {
-    return  async (dispatch : Dispatch<Action> ) => {
+export const searchRepositories = (term: string, numberOfrecord: number, country: string) => {
+    return async (dispatch: Dispatch<Action>) => {
         dispatch({
-            type : ActionType.SEARCH_REPOSITORIES
+            type: ActionType.SEARCH_REPOSITORIES
         });
-
+        term = term.replace(' ', '+');
+        let endPoint: string = '';
+        if (term !== '' && country === '') {
+            endPoint = `https://itunes.apple.com/search?term=${term}&limit=${numberOfrecord}`
+        }
+        else if (term !== '' && country !== '') {
+            endPoint = `https://itunes.apple.com/search?term=${term}&limit=${numberOfrecord}&country=${country}`
+        }
         try {
-            const {data} = await axios.get(`https://itunes.apple.com/search?term=${term}+johnson&country=${country}&limit=${numberOfrecord}&&entity=musicVideo`,{
-                params :{
-                    text : term
-                }
-            });
-            debugger;
+            const { data } = await axios.get(endPoint);
             // Added 1 second deley of time to visible shimmer effect only for first time. We can remove settimeout
-            numberOfrecord && numberOfrecord===10?  setTimeout(() => {
+            numberOfrecord && numberOfrecord === 10 ? setTimeout(() => {
                 dispatch({
-                    type : ActionType.SEARCH_REPOSITORIES_SUCCESS,
-                    payload : [data]
-                }) 
+                    type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+                    payload: [data]
+                })
                 return
-            }, 1000):
+            }, 1000) :
+                dispatch({
+                    type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+                    payload: [data]
+                })
+
+
+        } catch (error: any) {
             dispatch({
-                type : ActionType.SEARCH_REPOSITORIES_SUCCESS,
-                payload : [data]
-            }) 
-            
-            
-        } catch (error : any ) {
-            dispatch({
-                type : ActionType.SEARCH_REPOSITORIES_ERROR,
-                payload:error.message
+                type: ActionType.SEARCH_REPOSITORIES_ERROR,
+                payload: error.message
             })
         }
     }
